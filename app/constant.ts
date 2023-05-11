767a282303d232d938ede91db63be86b396fe48a
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-05-11 15:40:54 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,8 +1,8 @@
 export const OWNER = "Yidadaa";
 export const REPO = "Ai 百晓生";
-export const REPO_URL = `https://imgbed.link/file/22797`;
-export const ISSUE_URL = `https://imgbed.link/file/22797`;
+export const REPO_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=dUzJgExLN6BsYwrCDrZlSbZc06FxvfoR&authKey=EYXRcLQTpAX68ijIaWxiKFLEdsLyoQFqj12N7h16gLVSPZLWNCaeSlCwfeG9cnix&noverify=0&group_code=617648646`;
+export const ISSUE_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=dUzJgExLN6BsYwrCDrZlSbZc06FxvfoR&authKey=EYXRcLQTpAX68ijIaWxiKFLEdsLyoQFqj12N7h16gLVSPZLWNCaeSlCwfeG9cnix&noverify=0&group_code=617648646`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-export const FETCH_COMMIT_URL = `https://imgbed.link/file/22797`;
-export const FETCH_TAG_URL = `https://imgbed.link/file/22797`;
+export const FETCH_COMMIT_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=dUzJgExLN6BsYwrCDrZlSbZc06FxvfoR&authKey=EYXRcLQTpAX68ijIaWxiKFLEdsLyoQFqj12N7h16gLVSPZLWNCaeSlCwfeG9cnix&noverify=0&group_code=617648646`;
+export const FETCH_TAG_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=dUzJgExLN6BsYwrCDrZlSbZc06FxvfoR&authKey=EYXRcLQTpAX68ijIaWxiKFLEdsLyoQFqj12N7h16gLVSPZLWNCaeSlCwfeG9cnix&noverify=0&group_code=617648646`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
