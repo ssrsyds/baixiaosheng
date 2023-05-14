3c43ff4ae410aef0abdaab61ad3d70260eee0f99
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-05-14 15:42:30 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -124,7 +124,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "New Chat",
-    BotHello: "您好！尊贵的会员用户,有什么可以帮助你的吗？",
+    BotHello: "您好！尊贵的会员用户,有什么可以帮助你的吗？对你有帮助可[点我](http://aai.yjie.fun/ds/)打赏作者",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) =>
